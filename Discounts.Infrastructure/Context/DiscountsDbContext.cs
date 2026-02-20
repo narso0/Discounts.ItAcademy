c7898e0b679bd8065c3b3ef3f920069c005e3a75
Luka Narsia <lnarsia12@gmail.com>
2026-02-20 15:54:37 +0400

    Db Context main thing created

---
+++ Discounts.Infrastructure/Context/DiscountsDbContext.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Discounts.Domain.Entities;
+
+namespace Discounts.Infrastructure.Context;
+
+public class DiscountsDbContext : IdentityDbContext<IdentityUser>
+{
+    public DiscountsDbContext(DbContextOptions<DiscountsDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Discount> Discounts { get; set; }
+    public DbSet<Merchant> Merchants { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
+
+    protected override vodi OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        builder.Entity<Discount>()
+        .Property(p => p.OriginalPrice)
+        .HasPrecision(18,2);
+        builder.Entity<Discount>()
+        .Property(p => p.DiscountedPrice)
+        .HasPrecision(18,2);
+        builder.Entity<Reservation>()
+        .HasOne(x => x.Discount)
+        .WithMany(x => x.Reservations)
+        .OnDelete(DeleteBehavior.Restrict);
+    }
+}
